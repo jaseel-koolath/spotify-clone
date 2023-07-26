d6452e9b68952f3bdb089d7e9de6a22ef2c6f4ea
jaseel-koolath <112375897+jaseel-koolath@users.noreply.github.com>
2023-07-26 09:19:03 +0530

    Initial UI setup

---
+++ components/Box.tsx
@@ -0,0 +1,15 @@
+import { twMerge } from "tailwind-merge"
+
+interface BoxProps {
+  children: React.ReactNode
+  className?: string
+}
+
+const Box: React.FC<BoxProps> = ({children, className}) => {
+  console.log(className)
+  return (
+    <div className={twMerge("bg-neutral-900 h-fit w-full rounded-lg", className)}>{children}</div>
+  )
+}
+
+export default Box
